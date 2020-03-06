cc7ce5fc686201691235b7d13e26c3d5bec4436a
DivyaKolla <59427935+DivyaKolla@users.noreply.github.com>
2020-03-06 12:33:14 +0530

    hackthon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { PlaceOrderComponent } from './place-order/place-order.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-import { ViewOrderComponent } from './view-order/view-order.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    PlaceOrderComponent,
-    ContactUsComponent,
-    ViewOrderComponent
+    HomeComponent,  
+    ContactUsComponent
+   
   ],
   imports: [
     BrowserModule,
